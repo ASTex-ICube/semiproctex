be4304b17defbcfce50d05a2b1add9e2b3a7c506
Pascal Guehl <pascal.guehl@icube.unistra.fr>
2020-06-11 11:39:49 +0200

    update code (main file)

--- Semiproc_synthesis_Src/SptSynthesizer/main.cpp
+++ Semiproc_synthesis_Src/SptSynthesizer/main.cpp
@@ -14,6 +14,9 @@
  ******************************* INCLUDE SECTION ******************************
  ******************************************************************************/
 
+// Project
+#include "SptSynthesizer.h"
+
 // STL
 #include <string>
 #include <fstream>
@@ -51,17 +54,48 @@
  ******************************************************************************/
 int main( int pArgc, char** pArgv )
 {
+	// Log info
 	std::cout << "---------------------------------" << std::endl;
 	std::cout << "- SemiProcTex Synthesizer Tool --" << std::endl;
 	std::cout << "---------------------------------" << std::endl;
+
+	// Check command line arguments
+	const int nbArguments = 1;
+	if ( pArgc < ( 1 + nbArguments ) )
+	{
+		// Log info
+		std::cout << "Error: waiting for " << nbArguments << "parameter(s)" << std::endl;
+		std::cout << "       ex: program semiProcTex_params.txt"<< std::endl;
+
+		// Exit
+		return -1;
+	}
 
 	// Retrieve program directory
 	int indexParameter = 0;
 	std::string workingDirectory = "";
 	workingDirectory = pArgv[ indexParameter++ ];
 
-	// TODO
-	// ...
+	// User customizable parameters : retrieve command line parameters
+	const char* semiProcTexConfigFilename = pArgv[ indexParameter++ ];
 
+	// Initialization
+	Spt::SptSynthesizer* semiProcTexSynthesizer = new Spt::SptSynthesizer();
+	// - initialize resources
+	semiProcTexSynthesizer->initialize();
+
+	// Load synthesis parameters
+	semiProcTexSynthesizer->loadParameters( semiProcTexConfigFilename );
+
+	// Launch synthesis
+	semiProcTexSynthesizer->execute();
+	
+	// Finalization
+	// - clean/release resources
+	semiProcTexSynthesizer->finalize();
+	delete semiProcTexSynthesizer;
+	semiProcTexSynthesizer = nullptr;
+
+	// Exit
 	return 0;
 }
