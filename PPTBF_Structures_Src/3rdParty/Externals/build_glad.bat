983967364019b08777ee66b492496490ecac3795
Pascal Guehl <pascalguehl@yahoo.fr>
2020-10-20 10:25:18 +0200

    Update build_glad.bat

--- PPTBF_Structures_Src/3rdParty/Externals/build_glad.bat
+++ PPTBF_Structures_Src/3rdParty/Externals/build_glad.bat
@@ -1,3 +1,7 @@
 mkdir glad-build
 cd glad-build
+rem Call the script selecting the CMAKE compiler (Visual Studio 2010, 2012, etc...)
+rem "Visual Studio 14 2015 Win64"
+rem "Visual Studio 15 2017 Win64"
+rem "Visual Studio 16 2019 Win64"
 cmake -G "Visual Studio 15 2017 Win64" ..\glad
